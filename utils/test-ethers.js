7690cb2a9aae81272a03f7185c631c161f5ef0c8
Srinjoy Sen Chowdhury <116475469+AllMightLegend@users.noreply.github.com>
2024-09-02 00:56:37 +0530

    Your commit message here

--- utils/test-ethers.js
+++ utils/test-ethers.js
@@ -1,9 +1,9 @@
-import { ethers } from 'ethers';
+const { ethers } = require("ethers");
 
 const testEthers = () => {
   console.log('ethers:', ethers);
   try {
-    const provider = new ethers.providers.Web3Provider((window as any).ethereum);
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
     console.log('Provider created:', provider);
   } catch (error) {
     console.error('Error creating provider:', error);
