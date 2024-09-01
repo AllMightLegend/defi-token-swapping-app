b041dc346957295eb548083e66f35e01a6f66864
Srinjoy Sen Chowdhury <116475469+AllMightLegend@users.noreply.github.com>
2024-09-01 23:40:20 +0530

    Your commit message here

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,7 +15,11 @@ class MyDocument extends Document {
           <meta name="theme-color" content="#0070f3" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
+          <link 
+            rel="stylesheet" 
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" 
+          />
+          {/* Add other global stylesheets here */}
         </Head>
         <body>
           <Main />
